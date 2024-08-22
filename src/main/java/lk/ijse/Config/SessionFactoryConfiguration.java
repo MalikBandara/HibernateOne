94ef06eeb9029263be9583506d029cfa0b301b0b
Your Name <you@example.com>
2024-08-22 18:45:50 +0530

    Create session factory

---
+++ src/main/java/lk/ijse/Config/SessionFactoryConfiguration.java
@@ -0,0 +1,29 @@
+package lk.ijse.Config;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryConfiguration {
+
+    private static SessionFactoryConfiguration sessionFactoryConfiguration;
+
+    private SessionFactory sessionFactory;
+    private SessionFactoryConfiguration() {
+        Configuration configuration = new Configuration().configure();
+
+        sessionFactory=configuration.buildSessionFactory();
+    }
+
+    public static SessionFactoryConfiguration getInstance() {
+        if (sessionFactoryConfiguration==null){
+            sessionFactoryConfiguration = new SessionFactoryConfiguration();
+        }
+        return sessionFactoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+
+}
